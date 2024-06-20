788ac7fd49ed1327efe0d9b93615c9a3d4ebcc06
John Alvin Ambalong <119690431+jambalong@users.noreply.github.com>
2024-06-20 00:42:55 -0700

    Add #delete(data)

--- lib/tree.rb
+++ lib/tree.rb
@@ -37,10 +37,42 @@ class Tree
     root
   end
 
+  def delete(data, root = self.root)
+    return root unless root
+
+    case data <=> root.data
+    when -1
+      root.left = delete(data, root.left)
+    when 1
+      root.right = delete(data, root.right)
+    when 0
+      return root.right if root.left.nil?
+      return root.left if root.right.nil?
+
+      # Get the minimum value in the right subtree
+      root.data = min_value(root.right)
+      root.right = delete(root.data, root.right)
+    end
+
+    root
+  end
+
   def pretty_print(node = @root, prefix = '', is_left = true)
     pretty_print(node.right, "#{prefix}#{is_left ? '│   ' : '    '}", false) if node.right
     puts "#{prefix}#{is_left ? '└── ' : '┌── '}#{node.data}"
     pretty_print(node.left, "#{prefix}#{is_left ? '    ' : '│   '}", true) if node.left
+  end
+
+  private
+
+  def min_value(root)
+    min_value = root.data
+    while root.left
+      min_value = root.left.data
+      root = root.left
+    end
+
+    min_value
   end
 
 end
